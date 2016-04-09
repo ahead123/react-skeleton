0f639ffe3f8cd84287c50aa882f0681abf5aaf70
Arthur Head <ahead747@gmail.com>
2016-04-09 14:40:07 -0700

    added babel preset es2015 dependency

--- src/main.jsx
+++ src/main.jsx
@@ -4,4 +4,9 @@ var ReactDOM = require('react-dom');
 
 var List = require('./components/List.jsx');
 
-ReactDOM.render(<List />, document.getElementById('ingredients'));
+ReactDOM.render(
+
+	<List />, 
+	
+	document.getElementById('ingredients')
+);
